700abb775a6d683d8e5296846fa3613cc95f6714
Harsh Sharma <harshdsharma08@gmail.com>
2023-08-17 09:00:03 +0530

    FEAT: Add PositionRank, SingleRank, TopicRank, MultipartiteRank and YAKE models

---
+++ keyphrase_models/YAKE.py
@@ -0,0 +1,34 @@
+import spacy
+import pke
+
+nlp = spacy.load('en_core_web_sm')
+nlp.max_length = 2000000 # to get rid of max_length error which first came in paper_pk 2097
+extractor = pke.unsupervised.YAKE()
+
+def YAKE_keywords(text: str, type: str | None = None):
+    """
+    text: The text string
+    type: Can be either 'tit' for title or 'abs' for abstract. Anything else will be for pdf text.
+    """
+    if type == "tit":
+        top_n = 3
+    elif type == "abs":
+        top_n = 27
+    else:
+        top_n = 30
+
+    extractor.load_document(input=text, language='en', spacy_model=nlp)
+    extractor.candidate_selection()
+    extractor.candidate_weighting()
+    keyphrases = extractor.get_n_best(n=top_n)
+
+    result = []
+    for keyphrase in keyphrases:
+        if len(keyphrase[0]) <= 30: # there are some keywords which are more than 30 characters and seems useless. also this prevents 1000 char overflow in db
+            if "-" in keyphrase[0]:
+                result.append(keyphrase[0].replace("-", " "))
+            else:
+                result.append(keyphrase[0])
+
+    return result
+
--- keyphrase_models/__init__.py
+++ keyphrase_models/__init__.py
@@ -2,7 +2,12 @@ from keyphrase_models.keybert_model import keybert_keywords
 from keyphrase_models.keybart_model import keybart_keywords
 
 from keyphrase_models.TFIDF import TFIDF_keywords
+from keyphrase_models.YAKE import YAKE_keywords
 from keyphrase_models.TextRank import TextRank_keywords
+from keyphrase_models.SingleRank import SingleRank_keywords
+from keyphrase_models.TopicRank import TopicRank_keywords
+from keyphrase_models.PositionRank import PositionRank_keywords
+from keyphrase_models.MultipartiteRank import MultipartiteRank_keywords
 from keyphrase_models.KPMiner import KPMiner_keywords
 
 from keyphrase_models.bertkpe_model import bertkpe_keywords
@@ -10,6 +15,11 @@ from keyphrase_models.bertkpe_model import bertkpe_keywords
 model_dict = {"keybert":keybert_keywords,
             "keybart":keybart_keywords,
             "tfidf":TFIDF_keywords,
+            "yake":YAKE_keywords,
             "textrank":TextRank_keywords,
+            "singlerank":SingleRank_keywords,
+            "topicrank":TopicRank_keywords,
+            "positionrank":PositionRank_keywords,
+            "multipartiterank":MultipartiteRank_keywords,
             "kpminer":KPMiner_keywords,
             "bertkpe":bertkpe_keywords}
